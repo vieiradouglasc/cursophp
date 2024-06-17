95cc64669ae32d5bfde45246f9014cd314a7873b
vieiradouglasc <vieira.douglasc@gmail.com>
2024-06-17 13:05:10 -0300

    finalização array multidimensional com os dados dos valores do cardápio

--- exercicio2.php
+++ exercicio2.php
@@ -9,4 +9,12 @@
     $cardapioDia[4][1] = "Nhoque";
     $cardapioDia[5][1] = "Bacalhau";
     $cardapioDia[6][1] = "Feijão";
+
+    $cardapioDia[0][2] = "12,60";
+    $cardapioDia[1][2] = "10,00";
+    $cardapioDia[2][2] = "09,40";
+    $cardapioDia[3][2] = "11,20";
+    $cardapioDia[4][2] = "08,50";
+    $cardapioDia[5][2] = "15,20";
+    $cardapioDia[6][2] = "10,00";
 ?>
