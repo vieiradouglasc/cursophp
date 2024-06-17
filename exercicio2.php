e756149216aaf0616cb79c04e9862d148517f298
vieiradouglasc <vieira.douglasc@gmail.com>
2024-06-17 13:02:34 -0300

    finalizacao primeira parte array multidimensional com pratos do dia

--- exercicio2.php
+++ exercicio2.php
@@ -2,5 +2,11 @@
     $diaSemanaExtenso = date('l');
     $diaSemanaNumero = date('w');
 
-    
+    $cardapioDia[0][1] = "Lasanha";
+    $cardapioDia[1][1] = "Frango";
+    $cardapioDia[2][1] = "Arroz";
+    $cardapioDia[3][1] = "Feijoada";
+    $cardapioDia[4][1] = "Nhoque";
+    $cardapioDia[5][1] = "Bacalhau";
+    $cardapioDia[6][1] = "Feijão";
 ?>
