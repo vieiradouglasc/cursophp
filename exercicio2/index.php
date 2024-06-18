bf4a0cc12e36e593404038d375d6e81535a9c241
vieiradouglasc <vieira.douglasc@gmail.com>
2024-06-17 21:39:29 -0300

    Trocando <br> por <br>

--- exercicio2/index.php
+++ exercicio2/index.php
@@ -26,7 +26,7 @@
     ["12,60", "10,00", "09,40", "11,20", "08,50", "15,20", "10,00"]
     ];
     // Mensagem
-    echo "Hoje é " . $diaSemanaExtenso . "\n";
+    echo "Hoje é " . $diaSemanaExtenso . "<br>";
     echo "O prato do dia é " . $cardapioDia[0][$diaSemanaNumero] . " no valor de R$ " . $cardapioDia[1][$diaSemanaNumero] . "\n";
 
 ?>
