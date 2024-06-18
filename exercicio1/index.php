c003d3ea0fa7d9666d8d4a29dea2676a87214ca9
vieiradouglasc <vieira.douglasc@gmail.com>
2024-06-17 21:26:21 -0300

    Refatoração Exercício 1

--- exercicio1/index.php
+++ exercicio1/index.php
@@ -1,23 +1,27 @@
 <?php
+    
     $diaSemanaExtenso = date('l');
     $diaSemanaNumero = date('w');
 
-    $pratoDia[0] = "Lasanha";
-    $pratoDia[1] = "Frango";
-    $pratoDia[2] = "Arroz";
-    $pratoDia[3] = "Feijoada";
-    $pratoDia[4] = "Nhoque";
-    $pratodia[5] = "Bacalhau";
-    $pratoDia[6] = "Feijão";
+    // $pratoDia[0] = "Lasanha";
+    // $pratoDia[1] = "Frango";
+    // $pratoDia[2] = "Arroz";
+    // $pratoDia[3] = "Feijoada";
+    // $pratoDia[4] = "Nhoque";
+    // $pratodia[5] = "Bacalhau";
+    // $pratoDia[6] = "Feijão";
 
-    $valorPrato[0] = "12,60";
-    $valorPrato[1] = "10,00";
-    $valorPrato[2] = "09,40";
-    $valorPrato[3] = "11,20";
-    $valorPrato[4] = "08,50";
-    $valorPrato[5] = "15,20";
-    $valorPrato[6] = "10,00";
+    // $valorPrato[0] = "12,60";
+    // $valorPrato[1] = "10,00";
+    // $valorPrato[2] = "09,40";
+    // $valorPrato[3] = "11,20";
+    // $valorPrato[4] = "08,50";
+    // $valorPrato[5] = "15,20";
+    // $valorPrato[6] = "10,00";
+
+    $pratoDia = ["Lasanha", "Frango", "Arroz", "Feijoada", "Nhoque", "Bacalhau", "Feijão"];
+    $valorPrato = ["12,60", "10,00", "09,40", "11,20", "08,50", "15,20", "10,00"];
 
     echo "Hoje é " . $diaSemanaExtenso . "<br>";
-    echo "O prato é " . $pratoDia[$diaSemanaNumero] . " no valor de R$ " . $valorPrato[$diaSemanaNumero];
+    echo "O prato é " . $pratoDia[$diaSemanaNumero] . " no valor de R$ " . $valorPrato[$diaSemanaNumero]; 
 ?>
